96a4a5c010feea2ae939fe2aec6da1a827e4e2d9
Runa Ariga <harunaariga@gmail.com>
2021-11-17 19:40:42 -0800

    index added

--- 02-Homework/Develop/index.js
+++ 02-Homework/Develop/index.js
@@ -2,17 +2,38 @@
 const fs = require("fs");
 const util = require("util");
 const inquirer = require("inquirer");
-const generateReadme = require("./utils/generateReadme")
+const generateReadme = require(".utils/generateMarkdown.js")
 const writeFileAsync = util.promisify(fs.writeFile);
 
 // TODO: Create an array of questions for user input
-const questions = [];
+//const questions = [];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function promptUser(){
+    return inquirer.prompt([
+        {
+            type: "input",
+            name: "projectTitle",
+            message: "What is the project title?",
+        },
+    ]);
+} 
 
 // TODO: Create a function to initialize app
 function init() {}
 
 // Function call to initialize app
-init();
+async function init() {
+    try {
+        // Ask user questions and generate responses
+        const answers = await promptUser();
+        const generateContent = generateReadme(answers);
+        // Write new README.md to dist directory
+        await writeFileAsync('./dist/README.md', generateContent);
+        console.log('✔️  Successfully wrote to README.md');
+    }   catch(err) {
+        console.log(err);
+    }
+  }
+  
+  init();  
